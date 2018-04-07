515d2bcfb40099295e84fe0a84c7f3b223586c17
francois-ries <37154636+francois-ries@users.noreply.github.com>
2018-04-07 12:30:34 -0700

    Update UserBooking.php

--- UserBooking.php
+++ UserBooking.php
@@ -31,49 +31,65 @@
     <th>End Time</th> 
   </tr>
   <tr> 
-    <td><input id="start_time" type="time"></td>
-    <td><input id="end_timetime" type="time"></td> 
+    <td><input name="start_time" id="start_time" type="time"></td>
+    <td><input name="end_time" id="end_time" type="time"></td> 
   </tr>
 </table>
 
 
 <table style="margin: 0px auto;" >
-	<tbody>
 		<tr>
 			<td><h5>Room Equipment</h5> </td>
 			<td><h5>Campus Location</h5> </td>
 			<td><h5>Room Size</h5> </td>
 		</tr>
 		<tr>
-			<td><input type="radio" name="computer"> Computers </td>
-			<td><input type="radio" name="RN"> Rober North </td>
-			<td> <input type="radio" name="under_20"> Under 20 </td>
+			<td><input type="radio" id="computer" name="computer"> Computers </td>
+			<td><input type="radio" id="RN" name="RN"> Rober North </td>
+			<td> <input type="radio" id="under_20" name="under_20"> Under 20 </td>
 		</tr>
 		<tr>
-			<td><input type="radio" name="blackboard" > Blackboard </td>
-			<td><input type="radio" name="RS" > Robert South </td>
-			<td><input type="radio" name="20-40"> 20-40</td>
-		</tr>
-		<tr>
-			<td> </td>
-			<td><input type="radio" name="BC" > Bauer Center </td>
-			<td> <input type="radio" name="41-60"> 41-60 </td>
+			<td><input type="radio" id="blackboard" name="blackboard" > Blackboard </td>
+			<td><input type="radio" id="RS" name="RS" > Robert South </td>
+			<td><input type="radio" id="20-40" name="20-40"> 20-40</td>
 		</tr>
 		<tr>
 			<td> </td>
-			<td> <input type="radio" name="KS" > Kravis Center </td>
-			<td><input type="radio" name="above_60"> Above 60 </td>
+			<td><input type="radio" id="BC" name="BC" > Bauer Center </td>
+			<td> <input type="radio" id="41-60" name="41-60"> 41-60 </td>
 		</tr>
 		<tr>
 			<td> </td>
-			<td> </td>
-			<td> </td>
-		</tr>
-	</tbody>
+			<td> <input type="radio" id="KS" name="KS" > Kravis Center </td>
+			<td><input type="radio" id="above_60" name="above_60"> Above 60 </td>
+    </tr>
 </table>
-
-
+<center> <input type="submit"> </center>
 </form>
+
+<!-- PHP -->
+<?php
+
+// Assigne variables
+$start_time = null;
+if (isset($_POST["start_time"])) {
+  $start_time = trim($_POST["start_time"]);
+}
+$end_time = null;
+if (isset($_POST["end_time"])) {
+  $end_time = trim($_POST["end_time"]);
+}
+
+// Stores in array the if a specific box has been selected
+$formID_array = array ("computer"=>0, "RN"=>0, "under_20"=>0, "blackboard"=>0, "RS"=>0, "20-40"=>0, "BC"=>0, "41-60"=>0, "KS"=>0, "above_60"=>0);
+foreach ($formID_array as $key=>$value) {
+  if (isset($_POST[$key])) {
+    $formID_array[$key] = 1;
+  }
+}
+
+
+?>
 
 
 
@@ -104,17 +120,6 @@
 </center>
 
 
+
 </body></html>
 
-<!-- 
-<table style="margin: 0px auto;">
-  <tr>
-    <th>Start Time</th>
-    <th>End Time</th> 
-  </tr>
-  <tr> 
-    <td><input id="start_time" type="time"></td>
-    <td><input id="end_timetime" type="time"></td> 
-  </tr>
-</table>
--->
