bf06e4d7ca18b94b8caea870d989f716e5b915f4
Klaudia Dziewulski <35879785+klauds96@users.noreply.github.com>
2018-04-10 14:33:19 -0700

    Update adminview.php

--- view/adminview.php
+++ view/adminview.php
@@ -21,45 +21,34 @@ echo $output;*/
 <!DOCTYPE html>
 
 <?php 
-	if (isset($_SESSION['userid'])) {
-		$sid = $_SESSION['userid'];
-	}
-
-	$query = "SELECT fname from Customer where sid=?"; //select name from table if it matches input that was given
-	if($selectUser = $connection -> prepare($query)){ //makes sure prepare is not false
-		$selectUser -> bind_param("i", $sid);
-	}
-	if(!$selectUser) die ("Database access failed: " . $connection->error);
-	mysqli_stmt_execute($selectUser);
-    $selectUser -> bind_result($sid);
-    if($selectUser -> fetch()){ //means user is already in database, prints user name if already in db
-    	echo "Your name is $username <br>"; 
-    }else{
-    	echo "User not found <br>";
-    }
-    mysqli_stmt_close($selectUser);
+	$sid = $_SESSION['userid'];
 ?>
 
 	<head> 
 		<Title> Admin Main Page</Title>
     	<meta name="description" content="Main admin page where admin can view all current reservations.">
-    	<style>
-    		.nav{
-    			display: inline;
-    			padding: 8px;
-    			background-color: darkcyan;
-    		}
-    	</style>
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	</head>
 
 	<header>
 		<!---creates a navigator which links to main page or reservation page-->
-		<ul style="list-style-type: none; margin:0; padding:0">
-			<li class="nav"><a href="adminview.php">All Reservations</a></li>
-			<li class="nav"><a href="adminpending.php">Pending Reservations</a></li> 
-			<li class="nav"><a href="adminEdit.php">Room Updates</a></li>
-		</ul>
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">CMC Room Reservations</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="userview.php">Current Reservations</a></li>
+		      <li><a href="UserBooking.php">Search</a></li>
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right">
+		      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log out </a></li>
+		    </ul>
+		  </div>
+		</nav>
 	</header>
 
 	<body>
@@ -74,6 +63,7 @@ echo $output;*/
 				<th>Action</th>
 			</tr>
 			<?php
+			//$query= SELECT R.roomname, R.rid, O.rid FROM R.Room, O.Reservation WHERE R.rid=O.rid && approved=TRUE;
 			foreach($this->order as $key => $value){//iterate through reservations
 		          echo "<tr><td>".$roomname."</td>"; //room name
 		          echo "<td>".ShoppingCart::$cookieTypes[$key]."</td>"; //date of reservation
