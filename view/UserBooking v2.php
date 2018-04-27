498938b7c36d942a3b6343087c22ab66fe412b00
francois-ries <37154636+francois-ries@users.noreply.github.com>
2018-04-27 13:53:56 -0700

    Update UserBooking v2.php

--- view/UserBooking v2.php
+++ view/UserBooking v2.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <!-- saved from url=(0057)https://getbootstrap.com/docs/4.0/examples/sticky-footer/ -->
+
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
+<head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -14,11 +15,35 @@
 
     <!-- Custom styles for this template -->
     <link href="./Sticky Footer Template for Bootstrap_files/sticky-footer.css" rel="stylesheet">
-    <link href="grid.css" rel="stylesheet">
-
+    <!-- <link href="grid.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="UserBookingStyle.css" rel="stylesheet">
-
 </head>
+
+<header>
+		<!---creates a navigator which links to main page or reservation page-->
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">CMC Room Reservations</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+              <li><a href="userview.php">Current Reservations</a></li>
+              <li ><a href="AdminUpdateRoomFeatures.php">Update Room Features</a></li>
+		          <li class="active" ><a href="UserBooking.php">Make A Reservation</a></li>
+        </ul>
+		    <ul class="nav navbar-nav navbar-right">
+		      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log out </a></li>
+		    </ul>
+		  </div>
+		</nav>
+</header>
+
+
+
+
 <center><h1 text-align:"center">Make A Reservation</h1></center>
 <body>
 
@@ -254,7 +279,7 @@ $needBlackboard = null;
     //mail("fries19@cmc.edu","CS135",$msg);
   }
 
-  sendEmail(1,1);
+  //sendEmail(1,1);
 
 ?>
 
