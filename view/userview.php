4858890e57b23ae2d4903058bd9b2b2d71dc2ef6
Klaudia Dziewulski <35879785+klauds96@users.noreply.github.com>
2018-04-30 12:02:26 -0700

    Update userview.php

--- view/userview.php
+++ view/userview.php
@@ -11,7 +11,6 @@ print_r($_SESSION);
 	if (isset($_SESSION['userid'])) {
 		$user_id = $_SESSION['userid'];
  	}
- 	$user_id="30322200";
 
  	$servername = "localhost";
 	$username = "root";
@@ -29,15 +28,47 @@ print_r($_SESSION);
 
 	//finds all pending and accepted reservations with user with userid $user_id
 	function hasRoom ($con, $user_id) {
-		$query = "SELECT R.roomname, R.room_id, O.room_id, U.user_id, O.start_time, O.end_time, R.computer, R.blackboard FROM Room R, Reservation O, User U WHERE R.room_id=O.room_id AND R.user_id=U.user_id AND U.user_id=?";
+		$query = "SELECT R.roomname, R.room_id, O.room_id, U.user_id, O.start_time, O.end_time, R.computer, R.blackboard, O.approved, O.res_id FROM Room R, Reservation O, User U WHERE R.room_id=O.room_id AND O.user_id=U.user_id AND U.user_id=?";
 		if($getRoom = $con->prepare($query)){
-			$getRoom->execute($user_id);
+			$getRoom->execute(array($user_id));
 		}
 		$rooms = $getRoom->fetchAll();
 		return $rooms;
 	 
 	  }
 	  $rooms = hasRoom($con, $user_id);
+
+  	function getName($con, $user_id){
+		$query = "SELECT fname, lname FROM User WHERE user_id=?";
+		if($getName = $con->prepare($query)){
+			$getName->execute(array($user_id));
+		}
+		$currentUser = $getName->fetchAll();
+		return $currentUser;
+	}
+
+	$current = getName($con, $user_id);
+	$currentUser;
+
+	foreach($current as $array){
+		$fname = $array["fname"];
+		$lname = $array["lname"];
+		$currentUser = $fname." ".$lname;
+		echo $currentUser;
+	}
+
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		foreach($rooms as $array){
+			$thisReservation = $array["res_id"];
+			if(isset($_POST["cancel".$thisReservation])){
+				$query = "DELETE FROM Reservation WHERE res_id=?";
+				if($cancelReservation = $con->prepare($query)){
+					$cancelReservation->execute(array($thisReservation));
+				}
+			}
+		}
+		$rooms = hasRoom($con, $user_id);
+	}
 
 ?>
 
@@ -65,16 +96,16 @@ print_r($_SESSION);
 		    </ul>
 		  </div>
 		</nav>
-		<h1> Welcome <?php echo $username; ?> </h1>
+		<h1> Welcome <?php echo $currentUser; ?> </h1>
 	</header>
 
 	<body>
 		<h2>Reservations </h2>
+		<form method = "post">
 		<table width = 80%, class="table table-striped">
 			<tr> 
 				<th>Room</th>
-				<th>Date</th>
-				<th>Time</th>
+				<th>Date and Time</th>
 				<th>Room Features</th>
 				<th>Status</th>
 				<th>Action</th>
@@ -83,26 +114,40 @@ print_r($_SESSION);
 				foreach ($rooms as $array) {
 					$thisRoomId = $array["room_id"];
 					$thisRoomName = $array["roomname"];
-					$thisRoomStart = $arry["start_time"];
+					$thisRoomStart = $array["start_time"];
 					$thisRoomEnd = $array["end_time"];
 					$thisRoomComputer = $array["computer"];
 					$thisRoomBlackboard = $array["blackboard"];
 					$thisRoomApproved = $array["approved"];
+					$thisReservation = $array["res_id"];
 					$status = "";
+					$computer = "";
+					$blackboard = "";
 					if($thisRoomApproved == true){
 						$status = "Accepted";
 					}else{
 						$status = "Pending";
 					}
+					if($thisRoomComputer == 1){
+						$computer = "yes";
+					}else{
+						$computer = "no";
+					}
+					if($thisRoomBlackboard == 1){
+						$blackboard = "yes";
+					}else{
+						$blackboard = "no";
+					}
 					echo "<tr><td>".$thisRoomName."</td>"; //room name
-					echo "<td>Add date here </td>"; //date of reservation
-					echo "<td>".date("jS \of F, Y h:ia", strtotime($thisRoomStart))."to".date("jS \of F, Y h:ia", strtotime($thisRoomEnd))."</td>"; //time of reservation
-					echo "<td> Computer: ".$thisRoomComputer." Blackboard: ".$thisRoomBlackboard."</td>"; //room features
+					//echo "<td>Add date here </td>"; //date of reservation
+					echo "<td>".date("jS \of F, Y h:ia", strtotime($thisRoomStart))." to ".date("jS \of F, Y h:ia", strtotime($thisRoomEnd))."</td>"; //time of reservation
+					echo "<td> Computer: ".$computer."<br> Blackboard: ".$blackboard."</td>"; //room features
 					echo "<td>".$status."</td>"; //status of resrervation
-					echo "<td><input type='submit' name='cancel".$thisRoomId."value='Cancel'/></td>"; //cancel reservation
+					echo "<td><input type='submit' name='cancel".$thisReservation."'value='Cancel'/></td>"; //cancel reservation
 					echo "</tr>";
 				}
 			?>
 		</table>
+	</form>
 </body>
 
